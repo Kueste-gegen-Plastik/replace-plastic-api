0c7fcd7ca90a84e12ff5dff717263864f052944c
Felix Wahner <felix.wahner@neuwaerts.de>
2017-05-30 22:24:32 +0200

    listen only to localhost

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+const server = app.listen(port, 'localhost');
 
 server.on('listening', () =>
   console.log(`Feathers application started on ${app.get('host')}:${port}`)
