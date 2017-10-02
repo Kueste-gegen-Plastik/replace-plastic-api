a950df9b1ff3efd0ea30f18d0e49f64f672f1cb2
Felix Wahner <felix.wahner@neuwaerts.de>
2017-10-02 16:47:49 +0200

    Mails: Adjust the MailCron to aggregate products from a vendor if multiple of the same were scanned

--- src/custom/mailcron.js
+++ src/custom/mailcron.js
@@ -6,6 +6,7 @@ const remindertemplate = swig.compileFile(__dirname + '/../mails/reminder.swig')
 const htmlToText = require('html-to-text');
 const winston = require('winston');
 
+
 class MailCron {
 
   /**
@@ -45,13 +46,74 @@ class MailCron {
    * Fires the process to send mails
    * @returns {Promise}
    */
-  fire() {
-    if(!this.active) return;
-    return this.checkMailsToSend().then(mails => {
-      return this.sendMails(mails);
-    }).catch(err => {
+  async fire() {
+    if (!this.active) return;
+    try {
+      const mails = await this.checkMailsToSend();
+      if(!mails.length) return;
+      this.sendMails(mails);
+    } catch(err) {
       this.log("error", "Error in checkMailsToSend" + err.message);
+    }
+  }
+
+
+  async getVendorById(id) {
+    return this.app.service('vendors').get(id).then(vendor => {
+      if (!vendor || !vendor.hasOwnProperty('email') || !vendor.email) {
+        throw new Error('No valid vendor Data');
+      }
+      // get all entries that aren't sent yet
+      return vendor;
     })
+  }
+
+  async getEntriesForProducts(ids) {
+    return this.app.service('entries').find({
+        query: {
+          ProductId: ids,
+          sent: 0
+        },
+        group: ['ProductId']
+    });
+  }
+
+
+  getAllVendorMails(id) {
+    return this.app.service('mails').find({
+        query: {
+          vendorId: id,
+          sent: 0
+        },
+        attributes: ['id']
+    });
+  }
+
+  buildMailData(entries) {
+      let mailEntries = [];
+      // combine data for the mail
+      entries.data.forEach(entry => {
+        let idx = 0;
+        var itm = mailEntries.filter((entryItem, idx) => {
+          if(entryItem.ProductId === entry.ProductId || entryItem.barcode === entry.barcode) {
+            idx = idx;
+            return true
+          }
+          return false;
+        });
+        if(!itm.length) {
+          mailEntries.push({
+            barcode: entry.barcode,
+            ProductId: entry.id,
+            count: 1,
+            barcode: entry.barcode,
+            productname: `${entry['Product.name']} - ${entry['Product.detailname']}`
+          });
+        } else {
+          mailEntries[idx].count++;
+        }
+      });
+      return mailEntries;
   }
 
   /**
@@ -61,63 +123,72 @@ class MailCron {
    * @param {Array} mails The Mail-Entrys to be sent
    * @returns undefined
    */
-  sendMails(mails) {
-    // iterate each mail to send
-    mails.forEach(mail => {
-      // get the vendor contact
-      let foundEntries = [], foundVendor;
-      this.app.service('vendors').get(mail.vendorId).then(vendor => {
-        if(!vendor || !vendor.hasOwnProperty('email') || !vendor.email) {
-          throw new Error('No valid vendor Data');
+  async sendMails(mails) {
+    let foundEntries = [],
+        foundVendor;
+
+    const vendors = new Set(mails.map(mail => mail.vendorId));
+
+    for(let vendorid of vendors) {
+
+      try {
+        const vendor = await this.getVendorById(vendorid);
+        const vendorMails = await this.getAllVendorMails(vendorid);
+        if(!vendorMails.data.length) {
+          throw new Error(`No Mails found for vendor ${vendorid}`);
         }
-        // get all entries that aren't sent yet
-        foundVendor = vendor;
-        return this.app.service('entries').find({
-          query: {
-            ProductId: mail.productId,
-            sent: 0
-          }
-        });
-      }).then(entries => {
-        // save the found entries to reuse them later
-        foundEntries = entries.data;
-        if(!foundEntries.length) return Promise.reject('No entries found');
-        // get the product details for the mail
-        return this.app.service('product').get(mail.productId);
-      }).then(product => {
-        if(!product || !product.hasOwnProperty('detailname') || !product.detailname) {
-          throw new Error('No valid product found');
+
+        const mailProductIds = vendorMails.data.map(mail => mail.productId);
+        const entries = await this.getEntriesForProducts(mailProductIds);
+        if(!entries.data.length) {
+          throw new Error(`No Entries found ${vendorid}`);
         }
-        // create the mailtext from a swig template
+
+        foundEntries = this.buildMailData(entries);
         let mailText = vendortemplate({
-          entriesamount: foundEntries.length,
-          product: product.detailname
+          foundEntries: foundEntries,
+          productsString: foundEntries.map(product => product.productname).join(', '),
+          fullAmount: foundEntries.reduce((a,b) => {
+            if(a.hasOwnProperty('count')) { // first iteration
+              return (a.count ? parseInt(a.count,10) : 0) + parseInt(b.count,10);
+            }
+            return parseInt(a, 10) + parseInt(b.count,10);
+          })
         });
-        // send the mail via nodemailer
-        return this.mailer.sendMail(Object.assign(this.config, {
-            to: foundVendor.email,
-            html: mailText,
-            text: htmlToText.fromString(mailText)
+
+        var mail = await this.mailer.sendMail(Object.assign(this.config, {
+          to: typeof(process.env.NODE_ENV) !== 'undefined' && process.env.NODE_ENV === 'development' ? this.config.reminderEmail : vendor.email,
+          html: mailText,
+          text: htmlToText.fromString(mailText)
         }));
-      }).then(res => {
-        this.log("info", `Mail sent to: ${res.accepted.join(',')} (messageId: ${res.messageId})`);
-        return this.app.service('mails').patch(mail.id, {
-          sent : 1
+        if(!mail.accepted.length) {
+          throw new Error(`No Mails could be sent to found ${vendor.email}`);
+        }
+        this.log("info", `Mail sent to: ${mail.accepted.join(',')} (messageId: ${mail.messageId})`);
+        let mailsToPatch = vendorMails.data.map(mail => {
+          return mail.id;
         });
-      }).then(res => {
-        // set the status of all entries to "sent" so they won't
-        // be counted again in the next mail
-        return Promise.all(foundEntries.map(entry => {
-          return this.app.service('entries').patch(entry.id, {
-            sent: 1
-          });
-        }));
-      }).then(res => {
-        this.log("info", "++++++ Mails sent successfully ++++++");
-      }).catch(err => {
-        this.log("error", "Failed sending Mail:" + err.message);
-      });
-    });
+        await this.app.service('mails').patch(mailsToPatch, {
+          sent: 1,
+          reminded: 1
+        });
+        this.log("info", `Mails patched (sent): ${mailsToPatch.join(',')}`);
+        let entriesToPatch = entries.data.map(entry => {
+          return entry.id;
+        });
+        await this.app.service('entries').patch(entry.id, {
+          sent: 1
+        });
+        this.log("info", `Entries patched (sent): ${entriesToPatch.join(',')}`);
+
+        return foundEntries;
+
+      } catch(err) {
+        console.log("ERROR!", err);
+      }
+
+    }
+
   }
 
 
@@ -125,7 +196,7 @@ class MailCron {
   /**
    * Searches for mails that need to be send
    */
-  checkMailsToSend() {
+  async checkMailsToSend() {
     return this.app.service('mails').find({
       query: {
         createdAt: {
@@ -135,20 +206,20 @@ class MailCron {
       }
     }).then(mails => {
       let retVal = [];
-      if(mails.hasOwnProperty('total') && mails.total > 0) {
+      if (mails.hasOwnProperty('total') && mails.total > 0) {
         // filter for those mails that have a product and a
         // vendor assigned to them
         retVal = mails.data.filter(mail => {
           return mail.productId && mail.vendorId;
         });
-        if(retVal.length < mails.data.length) {
+        /* if (retVal.length < mails.data.length) {
           // send mails to admin: reminder to add vendors to products
           this.sendReminderMails(mails);
-        }
+        } */
       }
       return retVal;
     }).catch(err => {
-        this.log("error", "Failed checking for Mails:" + err.message);
+      this.log("error", "Failed checking for Mails:" + err.message);
     });
   }
 
@@ -159,7 +230,7 @@ class MailCron {
       return mail && (!mail.productId || !mail.vendorId) && (!mail.reminded || mail.reminded === 0);
     });
 
-    if(!mailsToSend.length) return;
+    if (!mailsToSend.length) return;
 
     // fill email text with amount of mails to be completed
     let mailText = remindertemplate({
@@ -168,22 +239,22 @@ class MailCron {
 
     // send email
     this.mailer.sendMail(Object.assign(this.config, {
-        to: this.config.reminderEmail,
-        subject: `ReplacePlastic: ${mailsToSend.length} Zuordnungen fehlen.`,
-        html: mailText,
-        text: htmlToText.fromString(mailText)
+      to: this.config.reminderEmail,
+      subject: `ReplacePlastic: ${mailsToSend.length} Zuordnungen fehlen.`,
+      html: mailText,
+      text: htmlToText.fromString(mailText)
     })).then(res => {
       // set status of all emails to "reminded" to avoid
       // spamming the admin with reminders
       return Promise.all(mailsToSend.map(mail => {
         return this.app.service('mails').patch(mail.id, {
-          reminded : 1
+          reminded: 1
         });
       }));
     }).then(res => {
-        this.log("info", "++++++ Reminder Mail sent successfully ++++++");
+      this.log("info", "++++++ Reminder Mail sent successfully ++++++");
     }).catch(err => {
-        this.log("error", "Failed sending reminder mail:" + err.message);
+      this.log("error", "Failed sending reminder mail:" + err.message);
     });
 
   }
